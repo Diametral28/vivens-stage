97392e8eda0ec321f07cd2453f36665a555f0958
Ivàn Alfonso Villa Vàzquez <ivanalfonsovillavazquez@mbp-de-ivan.gdl.mex.ibm.com>
2019-01-08 08:39:02 -0600

    version 4.1 Promos diciembre

---
+++ ajax/ajaxButton.php
@@ -0,0 +1,192 @@
+<?php
+  $valor = $_POST['valor'];
+
+  switch($valor){
+      case 599 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="QxDorkPOHsZihJv9/eC7IpfDUJfqtxWnA/o4kSc66pJWxTUzdnKNBA=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Un Masaje de 50min"/>
+              <input name="referenceCode" type="hidden" value="1mas50min01Dic18"/>
+              <input name="amount" type="hidden" value="599.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="35577acadd97fdc5a49de9da3f22307afaf8a9af5ebeb3a0e3e6d66ab6a6815d" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 1018 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="aq5QJz98zsZQdEKLeW8sb2CKYo5paQPg5sR299OXD/BPdy0Bd/iXdQ=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Dos Masajes de 50min"/>
+              <input name="referenceCode" type="hidden" value="2mas50min02Dic18"/>
+              <input name="amount" type="hidden" value="1018.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="c5bcc643026fb7c03ce20e92b379889e832335225c426a6d282e852af463ad7a" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 1796 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="wxpY29I7pi/a133PGqCL+WTzzj529Rm+6oAQPFKtR1qHf2GnZCDlkw=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Cuatro Masajes de 50min"/>
+              <input name="referenceCode" type="hidden" value="4mas50min03Dic18"/>
+              <input name="amount" type="hidden" value="1796.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="9ad2666e77d9a6d79589e0364a6c1423a61eae8c1ca945dc84f4c1ba6814abeb" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 2394 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="pdYUJNXD0pDD37CYunsEsQz4vh7l3crYjLWcDGZjTB0ukWoDwKqa+Q=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Seis Masajes de 50min"/>
+              <input name="referenceCode" type="hidden" value="6mas50min04Dic18"/>
+              <input name="amount" type="hidden" value="2394.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="173464f85bc8ed7e9043844654eab514e16e967677ec6e17ca5cf4b1d5bd7fc2" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 899 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="bG2YHDKBFXgMDB0LoyAuTd3yHdaNKqVRCQ9zGhnlUv0gXcX0e4VqAg=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Un Masajes de 100min"/>
+              <input name="referenceCode" type="hidden" value="1mas100min01Dic18"/>
+              <input name="amount" type="hidden" value="899.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="37d9d0d5bf0890e37546f6987acb3222006c90a213e57bca6b0cce33fd8324be" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 1528 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="Z1qpymg1FsXmRPfa3BeuUmSZVy0Ja2StscyqLjjhpOOFA73eJd0/cg=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Dos Masajes de 100min"/>
+              <input name="referenceCode" type="hidden" value="2mas100min02Dic18"/>
+              <input name="amount" type="hidden" value="1528.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="bdc71c5f219c3979409390e37897ccdeae8c004db11363f0115b3a6189dbd3e9" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 2696 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="gaTO6SB0GzOA2oKX+6jpGnGbJnZgWX5ktCQEbnJzs3nTjb4RQmJDSg=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Cuatro Masajes de 100min"/>
+              <input name="referenceCode" type="hidden" value="4mas100min03Dic18"/>
+              <input name="amount" type="hidden" value="2696.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="ae188bce84240f27035d4fd123fea0948012034c70f286ed1aabedd422b366f1" type="hidden"/>
+            </form>
+        <?php
+      break;
+      case 3594 : 
+        ?>
+            <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
+              <input class="pay-button2" type="image" border="0" alt="" src="http://www.payulatam.com/img-secure-2015/boton_pagar_pequeno.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+              <input name="buttonId" type="hidden" value="Z0osfFymPdAvQttV6JI8QlOERiAeogj3iI7ijfvSiMvZAMdLlP5LoQ=="/>
+              <input name="merchantId" type="hidden" value="697516"/>
+              <input name="accountId" type="hidden" value="700525"/>
+              <input name="description" type="hidden" value="Seis Masajes de 100min"/>
+              <input name="referenceCode" type="hidden" value="6mas100min04Dic18"/>
+              <input name="amount" type="hidden" value="3594.00"/>
+              <input name="tax" type="hidden" value="0.00"/>
+              <input name="taxReturnBase" type="hidden" value="0.00"/>
+              <input name="currency" type="hidden" value="MXN"/>
+              <input name="lng" type="hidden" value="es"/>
+              <input name="approvedResponseUrl" type="hidden" value="https://www.vivens.mx/message/exitosa.html"/>
+              <input name="declinedResponseUrl" type="hidden" value="https://www.vivens.mx/message/error.html"/>
+              <input name="pendingResponseUrl" type="hidden" value="https://www.vivens.mx/message/pendiente.html"/>
+              <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+              <input name="buttonType" value="SIMPLE" type="hidden"/>
+              <input name="signature" value="ec7371a928529d09e3850916b48462a6e17a7503169d4cc6e8d9ad121edf44df" type="hidden"/>
+            </form>
+        <?php
+      break;
+
+  }
+?>
+
